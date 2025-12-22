d249617beda26eac7df374334ddd8381165afede
Ioana Diana Ghisoiu <ioanagh077@gmail.com>
2025-12-22 11:31:48 +0200

    utilizarea functiei shell pentru verificare DNS

--- ex3.sh
+++ ex3.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+ $3 = server DNS
+verifică_ip() {
+    local host=$1
+    local ip=$2
+    local dns=$3
+
+    
+    local real_ip=$(nslookup "$host" "$dns" 2>/dev/null | awk '/Address: / {print $2; exit}')
+
+    if [ "$real_ip" != "$ip" ]; then
+        echo "Bogus IP for $host in /etc/hosts!"
+    fi
+}
+
+
+cat /etc/hosts | while read ip host extra; do
+    
+    if [[ -z "$ip" || "$ip" == \#* || "$host" == *"localhost"* ]]; then
+        continue
+    fi
+    if [[ -z "$host" ]]; then
+        continue
+    fi
+
+    
+    verifică_ip "$host" "$ip" "8.8.8.8"
+done#!/bin/bash
 
 cat /etc/hosts | while read ip host extra; do
 
