b998e539ef56193285371e4fb449f09a41a4e555
Ioana Diana Ghisoiu <ioanagh077@gmail.com>
2026-01-08 13:24:42 +0200

    programul inital

---
+++ functie.sh
@@ -0,0 +1,18 @@
+                                                                      
+#!/bin/bash
+
+cat /etc/hosts | while read ip host extra; do
+
+        if [[ -z "$ip" || "$ip" == \#* || $host == *" localhost"* ]]; then
+                continue
+        fi
+        if [[ -z "$host" ]]; then 
+                continue
+        fi
+
+        real_ip=$(nslookup "$host" 8.8.8.8 2>/dev/null | awk '/Address: / {print $2; exit}')
+        if [ "$real_ip" != "$ip" ]; then
+                echo "Bogus IP for $host in /etc/hosts!"
+        fi
+done
+
