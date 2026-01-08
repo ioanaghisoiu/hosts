a82c35e80b494f90fe605ba1af29d76d18813e05
Ioana Diana Ghisoiu <ioanagh077@gmail.com>
2026-01-08 13:29:02 +0200

    verificare cu functie

--- functie.sh
+++ functie.sh
@@ -1,18 +1,26 @@
                                                                       
 #!/bin/bash
 
+
+verifică_ip() {
+    local host=$1
+    local ip=$2
+    local dns=$3
+    real_ip=$(nslookup "$host" "$dns" 2>/dev/null | awk '/Address: / {print $2; exit}')
+  if [ "$real_ip" != "$ip" ]; then
+        echo "Bogus IP for $host in /etc/hosts!"
+    fi
+}
+
 cat /etc/hosts | while read ip host extra; do
 
         if [[ -z "$ip" || "$ip" == \#* || $host == *" localhost"* ]]; then
                 continue
         fi
-        if [[ -z "$host" ]]; then 
-                continue
-        fi
+        if [[ -n "$host" ]]; then
 
-        real_ip=$(nslookup "$host" 8.8.8.8 2>/dev/null | awk '/Address: / {print $2; exit}')
-        if [ "$real_ip" != "$ip" ]; then
-                echo "Bogus IP for $host in /etc/hosts!"
-        fi
+        verifică_ip "$host" "$ip" "8.8.8.8"
+    fi
+        
 done
 
